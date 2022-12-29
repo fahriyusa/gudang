ce91c6a4f13dc15b7633315434bd2b0708e573a4
Fahri Yusa Utama <fahriyusautama.com>
2022-12-29 09:05:58 +0700

    first commit

---
+++ application/views/login.php
@@ -0,0 +1,35 @@
+<!-- navbar -->
+<nav class="navbar bg-light">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Gudang Abrak</span>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Sruktur</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Kunci</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Informasi</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!-- end navbar -->
+
+<!-- login -->
+<form target="">
+    <div class="mb-3">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" class="form-control" id="username" name="username">
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<!-- end login -->
